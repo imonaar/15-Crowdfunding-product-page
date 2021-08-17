61685177c3b29c467bb1632c7188c7245a701764
imonaar <kevinmonari22@gmail.com>
2021-08-17 11:43:03 +0300

    modal hell

--- src/script/handle-submit.js
+++ src/script/handle-submit.js
@@ -1,28 +1,29 @@
-
 const submitBtns =Array.from(document.querySelectorAll('.submit-btn'));
+const smallModal = document.querySelector('.js-small-modal')
+const jsModalWindow = document.querySelector('.js-dialog-modal-window')
+const smallModalBtn = document.querySelector('.js-small-modal-btn')
+const dialog = document.querySelector('.js-dialog')
+
+//responsible for updating the progress
+const jsBacked = parseInt(document.querySelector('.js-backed').textContent)
+const jsBackers = parseInt(document.querySelector('.js-backers').textContent)
+const progress = document.querySelector('.progress-bar')
+
+//set the progress value when the page loads
+progress.value =Math.floor(jsBacked/1000)
 
 
 const updateProgress=(amount)=>{
-    const jsBacked = parseInt(document.querySelector('.js-backed').textContent)
-    const jsBackers = parseInt(document.querySelector('.js-backers'))
-    const progress = document.querySelector('.progress-bar')
-
+    //thinking of using local storage here
     let newBacked = jsBacked + amount
     let newBackers = jsBackers+1
     let newProgress = Math.floor(newBacked/1000)
 
-    progress.value = newBacked
+    progress.value = newProgress
+    document.querySelector('.js-backed').textContent = newBacked
+    document.querySelector('.js-backers').textContent = newBackers
 }
 
-// const handleSubmit = () =>{
-//     let submittedPledge
-//     let rawPledge = parseInt(submitBtn.parentNode.previousElementSibling.children[0].value)
-//     let fixedPledge = parseInt(submitBtn.previousElementSibling.firstChild.nextSibling.textContent)
-
-//     rawPledge > fixedPledge ? submittedPledge = rawPledge : submittedPledge = fixedPledge
-
-//     updateProgress(submittedPledge)
-// }
 
 submitBtns.forEach((btn, indx)=>{
     btn.addEventListener('click',() =>{
@@ -33,7 +34,13 @@ submitBtns.forEach((btn, indx)=>{
         rawPledge > fixedPledge ? submittedPledge = rawPledge : submittedPledge = fixedPledge
     
         updateProgress(submittedPledge)
-    } )
+
+        smallModal.classList.add('show-modal')
+        jsModalWindow.classList.add('hide')
+    })
 })
 
-
+smallModalBtn.addEventListener('click', ()=>{
+    dialog.classList.remove('opened')
+    document.body.classList.remove('menu-active')
+})
